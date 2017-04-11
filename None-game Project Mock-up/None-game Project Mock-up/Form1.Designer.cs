9a2ea1990fec9ce6a0da4fd8d23d4525a9eff839
Nohbdy <Billen93@outlook.com>
2017-04-11 12:08:29 +0200

    Fin justering af mock-up

--- None-game Project Mock-up/None-game Project Mock-up/Form1.Designer.cs
+++ None-game Project Mock-up/None-game Project Mock-up/Form1.Designer.cs
@@ -32,22 +32,36 @@
             this.TidligTurn = new System.Windows.Forms.Button();
             this.NyTurn = new System.Windows.Forms.Button();
             this.Exit = new System.Windows.Forms.Button();
-            this.nySpiller = new System.Windows.Forms.Button();
+            this.spillerNy = new System.Windows.Forms.Button();
             this.spillerInfo = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.spillerTilbage = new System.Windows.Forms.Button();
             this.tidligTurnTilbage = new System.Windows.Forms.Button();
             this.nyTurnTilbage = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.turnNavn = new System.Windows.Forms.Label();
+            this.turnType = new System.Windows.Forms.Label();
+            this.antalHold = new System.Windows.Forms.Label();
+            this.sportsgren = new System.Windows.Forms.Label();
+            this.startDate = new System.Windows.Forms.Label();
+            this.endDate = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // Spiller
             // 
-            this.Spiller.Location = new System.Drawing.Point(775, 319);
+            this.Spiller.Location = new System.Drawing.Point(407, 295);
             this.Spiller.Name = "Spiller";
-            this.Spiller.Size = new System.Drawing.Size(102, 77);
+            this.Spiller.Size = new System.Drawing.Size(223, 75);
             this.Spiller.TabIndex = 0;
             this.Spiller.Text = "Spiller";
             this.Spiller.UseVisualStyleBackColor = true;
@@ -55,9 +69,9 @@
             // 
             // TidligTurn
             // 
-            this.TidligTurn.Location = new System.Drawing.Point(764, 46);
+            this.TidligTurn.Location = new System.Drawing.Point(407, 202);
             this.TidligTurn.Name = "TidligTurn";
-            this.TidligTurn.Size = new System.Drawing.Size(102, 86);
+            this.TidligTurn.Size = new System.Drawing.Size(223, 75);
             this.TidligTurn.TabIndex = 1;
             this.TidligTurn.Text = "Tidligere turneringer";
             this.TidligTurn.UseVisualStyleBackColor = true;
@@ -65,9 +79,9 @@
             // 
             // NyTurn
             // 
-            this.NyTurn.Location = new System.Drawing.Point(156, 46);
+            this.NyTurn.Location = new System.Drawing.Point(407, 110);
             this.NyTurn.Name = "NyTurn";
-            this.NyTurn.Size = new System.Drawing.Size(104, 86);
+            this.NyTurn.Size = new System.Drawing.Size(223, 75);
             this.NyTurn.TabIndex = 2;
             this.NyTurn.Text = "Ny Turnering";
             this.NyTurn.UseVisualStyleBackColor = true;
@@ -75,27 +89,27 @@
             // 
             // Exit
             // 
-            this.Exit.Location = new System.Drawing.Point(156, 362);
+            this.Exit.Location = new System.Drawing.Point(12, 458);
             this.Exit.Name = "Exit";
-            this.Exit.Size = new System.Drawing.Size(104, 77);
+            this.Exit.Size = new System.Drawing.Size(84, 40);
             this.Exit.TabIndex = 3;
             this.Exit.Text = "Exit";
             this.Exit.UseVisualStyleBackColor = true;
             this.Exit.Click += new System.EventHandler(this.Exit_Click);
             // 
-            // nySpiller
+            // spillerNy
             // 
-            this.nySpiller.Location = new System.Drawing.Point(775, 402);
-            this.nySpiller.Name = "nySpiller";
-            this.nySpiller.Size = new System.Drawing.Size(102, 77);
-            this.nySpiller.TabIndex = 4;
-            this.nySpiller.Text = "Ny spiller";
-            this.nySpiller.UseVisualStyleBackColor = true;
-            this.nySpiller.Click += new System.EventHandler(this.nySpiller_Click);
+            this.spillerNy.Location = new System.Drawing.Point(374, 222);
+            this.spillerNy.Name = "spillerNy";
+            this.spillerNy.Size = new System.Drawing.Size(102, 77);
+            this.spillerNy.TabIndex = 4;
+            this.spillerNy.Text = "Ny spiller";
+            this.spillerNy.UseVisualStyleBackColor = true;
+            this.spillerNy.Click += new System.EventHandler(this.nySpiller_Click);
             // 
             // spillerInfo
             // 
-            this.spillerInfo.Location = new System.Drawing.Point(904, 402);
+            this.spillerInfo.Location = new System.Drawing.Point(565, 222);
             this.spillerInfo.Name = "spillerInfo";
             this.spillerInfo.Size = new System.Drawing.Size(102, 77);
             this.spillerInfo.TabIndex = 5;
@@ -107,38 +121,17 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(806, 151);
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 84);
+            this.listBox1.Size = new System.Drawing.Size(356, 436);
             this.listBox1.TabIndex = 6;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(156, 149);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 7;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(156, 177);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 8;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(156, 205);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 22);
-            this.textBox3.TabIndex = 9;
-            // 
             // spillerTilbage
             // 
-            this.spillerTilbage.Location = new System.Drawing.Point(639, 402);
+            this.spillerTilbage.Location = new System.Drawing.Point(12, 458);
             this.spillerTilbage.Name = "spillerTilbage";
-            this.spillerTilbage.Size = new System.Drawing.Size(102, 77);
+            this.spillerTilbage.Size = new System.Drawing.Size(84, 40);
             this.spillerTilbage.TabIndex = 10;
             this.spillerTilbage.Text = "Tilbage";
             this.spillerTilbage.UseVisualStyleBackColor = true;
@@ -146,9 +139,9 @@
             // 
             // tidligTurnTilbage
             // 
-            this.tidligTurnTilbage.Location = new System.Drawing.Point(672, 151);
+            this.tidligTurnTilbage.Location = new System.Drawing.Point(12, 458);
             this.tidligTurnTilbage.Name = "tidligTurnTilbage";
-            this.tidligTurnTilbage.Size = new System.Drawing.Size(102, 86);
+            this.tidligTurnTilbage.Size = new System.Drawing.Size(84, 40);
             this.tidligTurnTilbage.TabIndex = 11;
             this.tidligTurnTilbage.Text = "Tilbage";
             this.tidligTurnTilbage.UseVisualStyleBackColor = true;
@@ -156,28 +149,178 @@
             // 
             // nyTurnTilbage
             // 
-            this.nyTurnTilbage.Location = new System.Drawing.Point(46, 141);
+            this.nyTurnTilbage.Location = new System.Drawing.Point(12, 458);
             this.nyTurnTilbage.Name = "nyTurnTilbage";
-            this.nyTurnTilbage.Size = new System.Drawing.Size(104, 86);
+            this.nyTurnTilbage.Size = new System.Drawing.Size(84, 40);
             this.nyTurnTilbage.TabIndex = 12;
             this.nyTurnTilbage.Text = "Tilbage";
             this.nyTurnTilbage.UseVisualStyleBackColor = true;
             this.nyTurnTilbage.Click += new System.EventHandler(this.nyTurnTilbage_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(31, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(31, 73);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 14;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(31, 129);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 15;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(31, 185);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 16;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(31, 241);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 17;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(31, 297);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 18;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(31, 353);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 19;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(31, 409);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 22);
+            this.textBox8.TabIndex = 20;
+            // 
+            // turnNavn
+            // 
+            this.turnNavn.AutoSize = true;
+            this.turnNavn.Location = new System.Drawing.Point(137, 20);
+            this.turnNavn.Name = "turnNavn";
+            this.turnNavn.Size = new System.Drawing.Size(139, 17);
+            this.turnNavn.TabIndex = 22;
+            this.turnNavn.Text = "Navnet på Turnering";
+            // 
+            // turnType
+            // 
+            this.turnType.AutoSize = true;
+            this.turnType.Location = new System.Drawing.Point(137, 76);
+            this.turnType.Name = "turnType";
+            this.turnType.Size = new System.Drawing.Size(104, 17);
+            this.turnType.TabIndex = 23;
+            this.turnType.Text = "Turneringstype";
+            this.turnType.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // antalHold
+            // 
+            this.antalHold.AutoSize = true;
+            this.antalHold.Location = new System.Drawing.Point(137, 132);
+            this.antalHold.Name = "antalHold";
+            this.antalHold.Size = new System.Drawing.Size(73, 17);
+            this.antalHold.TabIndex = 24;
+            this.antalHold.Text = "Antal Hold";
+            // 
+            // sportsgren
+            // 
+            this.sportsgren.AutoSize = true;
+            this.sportsgren.Location = new System.Drawing.Point(137, 188);
+            this.sportsgren.Name = "sportsgren";
+            this.sportsgren.Size = new System.Drawing.Size(78, 17);
+            this.sportsgren.TabIndex = 25;
+            this.sportsgren.Text = "Sportsgren";
+            // 
+            // startDate
+            // 
+            this.startDate.AutoSize = true;
+            this.startDate.Location = new System.Drawing.Point(137, 244);
+            this.startDate.Name = "startDate";
+            this.startDate.Size = new System.Drawing.Size(72, 17);
+            this.startDate.TabIndex = 26;
+            this.startDate.Text = "Start Dato";
+            // 
+            // endDate
+            // 
+            this.endDate.AutoSize = true;
+            this.endDate.Location = new System.Drawing.Point(137, 300);
+            this.endDate.Name = "endDate";
+            this.endDate.Size = new System.Drawing.Size(66, 17);
+            this.endDate.TabIndex = 27;
+            this.endDate.Text = "Slut Dato";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(137, 356);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 17);
+            this.label7.TabIndex = 28;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(137, 412);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 17);
+            this.label8.TabIndex = 29;
+            this.label8.Text = "label8";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(841, 92);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 22);
+            this.textBox9.TabIndex = 30;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1045, 510);
-            this.Controls.Add(this.nyTurnTilbage);
-            this.Controls.Add(this.tidligTurnTilbage);
-            this.Controls.Add(this.spillerTilbage);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.endDate);
+            this.Controls.Add(this.startDate);
+            this.Controls.Add(this.sportsgren);
+            this.Controls.Add(this.antalHold);
+            this.Controls.Add(this.turnType);
+            this.Controls.Add(this.turnNavn);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.nyTurnTilbage);
+            this.Controls.Add(this.tidligTurnTilbage);
+            this.Controls.Add(this.spillerTilbage);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.spillerInfo);
-            this.Controls.Add(this.nySpiller);
+            this.Controls.Add(this.spillerNy);
             this.Controls.Add(this.Exit);
             this.Controls.Add(this.NyTurn);
             this.Controls.Add(this.TidligTurn);
@@ -185,6 +328,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -196,15 +340,29 @@
         private System.Windows.Forms.Button TidligTurn;
         private System.Windows.Forms.Button NyTurn;
         private System.Windows.Forms.Button Exit;
-        private System.Windows.Forms.Button nySpiller;
+        private System.Windows.Forms.Button spillerNy;
         private System.Windows.Forms.Button spillerInfo;
         private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button spillerTilbage;
         private System.Windows.Forms.Button tidligTurnTilbage;
         private System.Windows.Forms.Button nyTurnTilbage;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label turnNavn;
+        private System.Windows.Forms.Label turnType;
+        private System.Windows.Forms.Label antalHold;
+        private System.Windows.Forms.Label sportsgren;
+        private System.Windows.Forms.Label startDate;
+        private System.Windows.Forms.Label endDate;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox9;
     }
 }
 
--- None-game Project Mock-up/None-game Project Mock-up/Form1.cs
+++ None-game Project Mock-up/None-game Project Mock-up/Form1.cs
@@ -13,36 +13,111 @@ namespace None_game_Project_Mock_up
 {
     public partial class Form1 : Form
     {
+        #region Fields
+
+        enum Menu : byte { mainMenu = 1, nyTurnMenu, tidligTurnMenu, spillerMenu, nySpillerMenu, infoSpillerMenu}
+
         Database database = new Database();
+
+        #endregion
+        Menu CurrentMenu = Menu.mainMenu;
+
         public Form1()
         {
-            InitializeComponent();
-            TidligTurn.Visible = true;
-            Spiller.Visible = false;
-            NyTurn.Visible = false;
-            Exit.Visible = true;
-            nySpiller.Visible = false;
-            spillerInfo.Visible = false;
-            spillerTilbage.Visible = false;
-            tidligTurnTilbage.Visible = false;
-            nyTurnTilbage.Visible = true;
-            listBox1.Visible = false;
             
+            InitializeComponent();
+
+            #region ButtonVisibility
+            TidligTurn.Visible = true;
+            Spiller.Visible = true;
+            NyTurn.Visible = true;
+            Exit.Visible = true;
+
+            turnNavn.Visible = false;
+            turnType.Visible = false;
+            antalHold.Visible = false;
+            sportsgren.Visible = false;
+            startDate.Visible = false;
+            endDate.Visible = false;
+            label7.Visible = false;
+            label8.Visible = false;
+
+            textBox1.Visible = false;
+            textBox2.Visible = false;
+            textBox3.Visible = false;
+            textBox4.Visible = false;
+            textBox5.Visible = false;
+            textBox6.Visible = false;
+            textBox7.Visible = false;
+            textBox8.Visible = false;
+
+            spillerNy.Visible = false; // Done
+            spillerInfo.Visible = false; // Done
+            spillerTilbage.Visible = false; // Done
+            tidligTurnTilbage.Visible = false; // Done
+            nyTurnTilbage.Visible = false; // Done  
+            listBox1.Visible = false; //Done
+            #endregion
+            BackColor = Color.Green;
         }
 
+
+        #region Methods
         private void button1_Click(object sender, EventArgs e)
         {
             //Spiller
+            spillerNy.Visible = true;
+            spillerInfo.Visible = true;
+            spillerTilbage.Visible = true;
+
+            TidligTurn.Visible = false;
+            Spiller.Visible = false;
+            NyTurn.Visible = false;
+            Exit.Visible = false;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             //Tidligere Turnering
+            TidligTurn.Visible = false;
+            Spiller.Visible = false;
+            NyTurn.Visible = false;
+            Exit.Visible = false;
+
+            tidligTurnTilbage.Visible = true;
+            listBox1.Visible = true;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
+            BackColor = Color.DarkGreen;
+
             // Ny Turnering
+            TidligTurn.Visible = false;
+            Spiller.Visible = false;
+            NyTurn.Visible = false;
+            Exit.Visible = false;
+
+            nyTurnTilbage.Visible = true;
+            textBox1.Visible = true;
+            textBox2.Visible = true;
+            textBox3.Visible = true;
+            textBox4.Visible = true;
+            textBox5.Visible = true;
+            textBox6.Visible = true;
+            textBox7.Visible = true;
+            textBox8.Visible = true;
+
+            turnNavn.Visible = true;
+            turnType.Visible = true;
+            antalHold.Visible = true;
+            sportsgren.Visible = true;
+            startDate.Visible = true;
+            endDate.Visible = true;
+            label7.Visible = true;
+            label8.Visible = true;
+
+
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -53,6 +128,7 @@ namespace None_game_Project_Mock_up
         private void Form1_Load(object sender, EventArgs e)
         {
             // Opret tabeller / databasser
+
             database.databaseRun();
         }
 
@@ -65,30 +141,98 @@ namespace None_game_Project_Mock_up
         private void nyTurnTilbage_Click(object sender, EventArgs e)
         {
             BackColor = Color.Green;
+
+            nyTurnTilbage.Visible = false;
+
+
+            TidligTurn.Visible = true;
+            Spiller.Visible = true;
+            NyTurn.Visible = true;
+            Exit.Visible = true;
+            textBox1.Visible = false;
+            textBox2.Visible = false;
+            textBox3.Visible = false;
+            textBox4.Visible = false;
+            textBox5.Visible = false;
+            textBox6.Visible = false;
+            textBox7.Visible = false;
+            textBox8.Visible = false;
+
+            textBox9.Visible = false; // bleh
+
+            turnNavn.Visible = false;
+            turnType.Visible = false;
+            antalHold.Visible = false;
+            sportsgren.Visible = false;
+            startDate.Visible = false;
+            endDate.Visible = false;
+            label7.Visible = false;
+            label8.Visible = false;
+
         }
 
         private void tidligTurnTilbage_Click(object sender, EventArgs e)
         {
+            tidligTurnTilbage.Visible = false;
+            listBox1.Visible = false;
+
+            TidligTurn.Visible = true;
+            Spiller.Visible = true;
+            NyTurn.Visible = true;
+            Exit.Visible = true;
 
         }
 
         private void spillerTilbage_Click(object sender, EventArgs e)
         {
+            spillerNy.Visible = false;
+            spillerInfo.Visible = false;
+            spillerTilbage.Visible = false;
+
+            TidligTurn.Visible = true;
+            Spiller.Visible = true;
+            NyTurn.Visible = true;
+            Exit.Visible = true;
 
         }
 
         private void nySpiller_Click(object sender, EventArgs e)
         {
 
+
         }
 
         private void spillerInfo_Click(object sender, EventArgs e)
         {
 
+
         }
+
+#endregion
+
+
         private void whatever()
         {
+
             //rnd(0, B);
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+            private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Enter && textBox9.Visible == true)
+            {
+
+                
+
+            }
+        }
+    
     }
 }
